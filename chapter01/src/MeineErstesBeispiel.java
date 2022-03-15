a5e1022a66bd4877ff532923e1fd3753f02ce16e
Marloto <marloto@codestack.de>
2022-03-15 14:36:52 +0100

    Introduction to Java

---
+++ chapter01/src/MeineErstesBeispiel.java
@@ -0,0 +1,19 @@
+class MeinErstesBeispiel {
+	public static void main(String[] args) {
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		int zahl2 = scanner.nextInt();
+		String eingabe = scanner.next();
+		
+		int zahl = 32; // lokale variable
+		String someString = "Test-Message";
+		
+		System.out.println("Hello, World!");
+		System.out.println(someString);
+		System.out.println(zahl);
+		System.out.println(eingabe);
+		System.out.println(zahl2);
+		
+		
+		// Methoden-Signatur
+	}
+}
