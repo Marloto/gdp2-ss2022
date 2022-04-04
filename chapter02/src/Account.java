7a15edd5d64e05d1b8469bcd678dfb41111d8b66
Sebastian Apel <marloto@codestack.de>
2022-04-04 17:21:44 +0200

    Account References

--- chapter02/src/Account.java
+++ chapter02/src/Account.java
@@ -33,4 +33,17 @@ public class Account {
     	System.out.print(balance);
     	System.out.println(" €.");
     }
+    
+    public static void main(String[] args) {
+        Account acc1 = new Account("DE...", 1234, 1.0);
+        Account acc2 = new Account("DE...", 1234, 1.0);
+        Account[] arr = new Account[]{acc1, acc2};
+        Account foo = acc2;
+        acc1 = acc2;
+        acc2 = foo;
+        acc1.withdraw(1);
+        for(Account a : arr) {
+        	a.print();
+        }
+	}
 }
